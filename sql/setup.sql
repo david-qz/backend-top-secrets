7a0dd94afb3de8f72d70c1f127ddd699bb970b54
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-08 14:42:13 -0700

    Add database set up script

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+drop table if exists users;
+drop table if exists secrets;
+
+create table users (
+    id bigint generated always as identity primary key,
+    email text not null,
+    password_hash text not null
+);
+
+create table secrets (
+    id bigint generated always as identity primary key,
+    title text not null,
+    description text not null,
+    created_at timestamp with time zone default CURRENT_TIMESTAMP
+);
+
+insert into secrets (title, description)
+values ('Lunch Menu', 'The cafeteria is serving tuna salad today.');
